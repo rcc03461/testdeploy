220965ee3dedb048d938ffd4022cd0462b1ab3d6
rcc03461 <ych8815@gmail.com>
2018-08-08 02:35:28 +0800

    Set allow origin to *

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //跨域问题处理(vue端使用了proxyTable下面的配置不用了额)
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', "http://cre.yching.hk");
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials','true'); //告诉客户端可以在HTTP请求中带上Cookie
