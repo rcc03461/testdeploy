65297b1166d13460a2dcf43d22a01bd4ea0dfb36
rcc03461 <ych8815@gmail.com>
2018-08-18 00:59:24 +0800

    add cors option

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -8,16 +8,16 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var router = express();
-const corsOptions = {
-  origin: 'http://104.155.212.227',
-  credentials: true
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-};
-router.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: 'http://104.155.212.227',
+//   credentials: true
+// //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+// //   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+// router.use(cors(corsOptions));
 
-// router.use(bodyParser.urlencoded({ extended: true }));
-// router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 var indexRouter = {};
 var userController = require('../../../controllers/user');
 
