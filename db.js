7195e911338df73203bff8268ecfa00f9e522fdc
rcc03461 <ych8815@gmail.com>
2018-08-18 03:54:26 +0800

    consolid all route

---
+++ db.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+  user: 'root',
+  password: 'hing8808',
+  database: 'cre',
+  host: 'localhost',
+})
+// const pool = mysql.createPool({
+//   user: 'root',
+//   password: '',
+//   database: 'cre',
+//   host: 'localhost',
+// })
+
+
+const query = function (sql, values) {
+
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        resolve(err)
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          console.log("exec : ", sql, ' ', new Date());
+          
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          connection.release()
+        })
+      }
+    })
+  })
+
+}
+
+
+module.exports = {
+  query,
+  pool
+}
